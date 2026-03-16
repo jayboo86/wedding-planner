0f5afa5dd281bc8d3aec6cb2ea3591163ad3f20f
jayboo86 <demoneon@gmail.com>
2026-03-16 17:44:25 -0500

    Fix: use valid bcrypt hashes for admin and nicole passwords

--- server/users.js
+++ server/users.js
@@ -2,8 +2,8 @@
 const users = [
   // Example user: username: 'admin', password: 'password' (hashed)
   // Passwords should be hashed using bcrypt
-  { username: 'admin', password: '$2b$10$Q9QwQwQwQwQwQwQwQwQwQeQwQwQwQwQwQwQwQwQwQwQwQwQwQwQ' }, // password: admin123
-  { username: 'nicole', password: '$2b$10$Qw8Qw8Qw8Qw8Qw8Qw8QwOeQw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8Qw8Q' }, // password: 142284
+  { username: 'admin', password: '$2b$10$UsccWWawCggwP2ByDom4meX2KnEqOwLByUVcNG.5LoUjsJhe2SLSi' }, // password: admin123
+  { username: 'nicole', password: '$2b$10$IomhF31cvmkn.fjK9VlcGefy7iBVArrAs5BgN41jMovXxfLhCkm.2' }, // password: 142284
 ];
 
 module.exports = users;
